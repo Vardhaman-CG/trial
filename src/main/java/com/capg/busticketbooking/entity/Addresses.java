cb657cdc5c1c171035b1219f90f3cdaecb4dc4ab
Naga Praharshini <praharshini.naga-praharshini@capgemini.com>
2025-11-27 15:59:45 +0530

    Entities customer and address

--- src/main/java/com/capg/busticketbooking/entity/Addresses.java
+++ src/main/java/com/capg/busticketbooking/entity/Addresses.java
@@ -1,5 +1,62 @@
-package com.capg.busticketbooking.entity;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Addresses {
+@Entity
+@Table(name = "addresses")
+public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Integer addressId;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(name = "zip_code", nullable = false)
+    private String zipCode;
+
+    // One address can belong to many customers
+    @OneToMany(mappedBy = "address", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Customer> customers = new ArrayList<>();
+
+    // One address can belong to many agency offices
+    @OneToMany(mappedBy = "address", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AgencyOffice> agencyOffices = new ArrayList<>();
+
+    // One address can belong to many drivers
+    @OneToMany(mappedBy = "address", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Driver> drivers = new ArrayList<>();
+
+    // --- Getters and Setters ---
+    public Integer getAddressId() { return addressId; }
+    public void setAddressId(Integer addressId) { this.addressId = addressId; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+
+    public String getZipCode() { return zipCode; }
+    public void setZipCode(String zipCode) { this.zipCode = zipCode; }
+
+    public List<Customer> getCustomers() { return customers; }
+    public void setCustomers(List<Customer> customers) { this.customers = customers; }
+
+    public List<AgencyOffice> getAgencyOffices() { return agencyOffices; }
+    public void setAgencyOffices(List<AgencyOffice> agencyOffices) { this.agencyOffices = agencyOffices; }
+
+    public List<Driver> getDrivers() { return drivers; }
+    public void setDrivers(List<Driver> drivers) { this.drivers = drivers; }
 }
