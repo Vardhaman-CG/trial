359d19bfc42a2bf4dbda6d3219f5e5f941b5819d
Sankeerthi2626 <chandrugonda.sankeerthi@capgemini.com>
2025-11-27 12:19:22 +0530

    Add files via upload
    add file

---
+++ Product.java
@@ -0,0 +1,19 @@
+package com.capg.springinti.entity;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+
+    // constructor
+    public Product(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // getters and setters
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+}
