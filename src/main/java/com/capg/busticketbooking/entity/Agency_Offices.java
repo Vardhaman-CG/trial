76d5df08566e1423d2752f1bbb2101ee8b8fc9a7
VARDHAMAN-CG <vardhaman.hiteshkumar-begani@capgemini.com>
2025-11-27 15:43:04 +0530

    added agencies and agency_office entities

--- src/main/java/com/capg/busticketbooking/entity/Agency_Offices.java
+++ src/main/java/com/capg/busticketbooking/entity/Agency_Offices.java
@@ -1,5 +1,30 @@
 package com.capg.busticketbooking.entity;
 
-public class Agency_Offices {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "agency_offices")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Agency_Offices {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer office_id;
+
+    @ManyToOne
+    @JoinColumn(name = "agency_id")
+    private Agencies agency_id;
+
+    private String office_mail;
+    private String office_contact_person_name;
+    private String office_contact_number;
+
+    @ManyToOne
+    @JoinColumn(name = "office_address_id")
+    private Addresses office_address_id;
 }
