661fcd35469eb66e8bba3edc7fcf9ba764732e36
praharshini3366 <praharshini.naga-praharshini@capgemini.com>
2025-11-28 13:01:51 +0530

    DTO added

---
+++ src/main/java/com/capg/busticketbooking/dto/AddressesDTO.java
@@ -0,0 +1,27 @@
+package com.capg.busticketbooking.dto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddressesDTO {
+        private Integer addressId;
+        private String address;
+        private String city;
+        private String state;
+        private String zipCode;
+
+        // Instead of full entities, use IDs or DTOs
+        private List<Integer> customerIds;
+        private List<Integer> agencyOfficeIds;
+        private List<Integer> driverIds;
+    }
+
